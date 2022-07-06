519a279bc2e1d6f22d16a574a0f591bd34e87139
Omar Cristhian Romero Chipana <oromero@unsa.edu.pe>
2022-07-06 16:52:12 -0500

    creando el objeto destino y enviandolos a la vista

--- proyects/telusko/travello/views.py
+++ proyects/telusko/travello/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
-
 def index(request):
-    return render(request,'index.html')
+    destino1 = Destination()
+    destino1.name = 'Destino 1'
+    destino1.price = 600
+
+    return render(request,'index.html',{
+        'dest1': destino1,
+    })
